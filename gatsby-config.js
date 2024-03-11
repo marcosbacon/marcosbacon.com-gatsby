9663a15522141da42e004cb927138962dc4325b9
Marcos Bacon <marcosbacon@Marcoss-Mac-mini.local>
2024-03-11 15:06:42 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Marcos Bacon`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
